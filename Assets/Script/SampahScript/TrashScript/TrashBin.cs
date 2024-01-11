bbbd077fbdbffc8689ba84be548dc657510f8a94
Valenstio <71970485+lasiest@users.noreply.github.com>
2024-01-11 23:10:25 +0700

    Adjustment-on-gui
    adjustment-on-gui
    
    + pop up information after discard waste
    + sampah information description

--- Assets/Script/SampahScript/TrashScript/TrashBin.cs
+++ Assets/Script/SampahScript/TrashScript/TrashBin.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class TrashBin : Trash
@@ -5,13 +6,15 @@ public class TrashBin : Trash
     private SampahInformation collectedTrashInfo;
 
     private readonly SampahInformation.tipeSampahEnum none = SampahInformation.tipeSampahEnum.None;
+    [SerializeField]private GameObject popUpBerhasil;
+    [SerializeField]private GameObject popUpGagal;
 
     private int SetScore() => SampahInformation.tipeSampah == none
         ? +collectedTrashInfo.poinSampah / 2
         : (collectedTrashInfo.tipeSampah == none
             ? +collectedTrashInfo.poinSampah / 2 
             : (collectedTrashInfo.tipeSampah == SampahInformation.tipeSampah
-                ? +collectedTrashInfo.poinSampah 
+                ? +collectedTrashInfo.poinSampah
                 : -collectedTrashInfo.poinSampah
             )
         );
@@ -22,8 +25,24 @@ public class TrashBin : Trash
         {
             var trashGrabbable = other.gameObject.GetComponent<TrashGrabbable>();
             collectedTrashInfo = trashGrabbable.SampahInformation;
+            if(sampahInformation.tipeSampah == collectedTrashInfo.tipeSampah){
+                StartCoroutine(ShowPopUp(true));
+            }else{
+                StartCoroutine(ShowPopUp(false));
+            }
             StageManagerScript.Instance.Increase?.Invoke(SetScore(), -1);
             Destroy(other.gameObject);
         }
+    }
+
+    IEnumerator ShowPopUp(bool isPopUpBerhasil){
+        if(isPopUpBerhasil){
+            popUpBerhasil.SetActive(true);
+        }else{
+            popUpGagal.SetActive(true);
+        }
+        yield return new WaitForSeconds(2f);
+        popUpBerhasil.SetActive(false);
+        popUpGagal.SetActive(false);
     }
 }
